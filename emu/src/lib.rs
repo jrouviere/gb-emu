a9d846a4c5494d858be8513f56bc8506908c4af3
Julien Rouviere <julien.rouviere@gmail.com>
2020-01-02 22:31:06 +0000

    Add some more instructions

--- emu/src/lib.rs
+++ emu/src/lib.rs
@@ -33,7 +33,22 @@ impl fmt::Display for Reg16 {
 
 #[derive(Copy, Clone)]
 enum Instruction {
-    Nop(),
+    ADDr8r8(Reg8, Reg8),
+    ADDr16r16(Reg16, Reg16),
+    ADDr16n8(Reg16, u8),
+    ADC(Reg8),
+    SBC(Reg8),
+    CP(Reg8),
+    SUB(Reg8),
+    AND(Reg8),
+    OR(Reg8),
+    XOR(Reg8),
+
+    INC8(Reg8),
+    INC16(Reg16),
+    DEC8(Reg8),
+    DEC16(Reg16),
+
     LD(Reg8, Reg8),
     LDrn(Reg8, u8),
     LDr8r16(Reg8, Reg16),
@@ -42,14 +57,27 @@ enum Instruction {
     LDr8n16(Reg8, u16),
     LDn16r8(u16, Reg8),
     LDr16n16(Reg16, u16),
-
+    Nop(),
     Unimplemented(u8),
 }
 
 impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Instruction::Nop() => write!(f, "NOP"),
+            Instruction::ADDr8r8(r1, r2) => write!(f, "ADD {},{}", r1, r2),
+            Instruction::ADDr16r16(r1, r2) => write!(f, "ADD {},{}", r1, r2),
+            Instruction::ADDr16n8(r16, n8) => write!(f, "ADD {},{}", r16, n8),
+            Instruction::INC8(r) => write!(f, "INC {}", r),
+            Instruction::INC16(r) => write!(f, "INC {}", r),
+            Instruction::DEC8(r) => write!(f, "DEC {}", r),
+            Instruction::DEC16(r) => write!(f, "DEC {}", r),
+            Instruction::ADC(r) => write!(f, "ADC {}", r),
+            Instruction::SBC(r) => write!(f, "SBC {}", r),
+            Instruction::CP(r) => write!(f, "CP {}", r),
+            Instruction::SUB(r) => write!(f, "SUB {}", r),
+            Instruction::AND(r) => write!(f, "AND {}", r),
+            Instruction::OR(r) => write!(f, "OR {}", r),
+            Instruction::XOR(r) => write!(f, "XOR {}", r),
             Instruction::LD(r1, r2) => write!(f, "LD {},{}", r1, r2),
             Instruction::LDrn(r, n) => write!(f, "LDrn {},{}", r, n),
             Instruction::LDr8r16(r8, r16) => write!(f, "LD {},({})", r8, r16),
@@ -58,7 +86,7 @@ impl fmt::Display for Instruction {
             Instruction::LDr8n16(r8, n16) => write!(f, "LD {},({})", r8, n16),
             Instruction::LDn16r8(n16, r8) => write!(f, "LD ({}),{}", n16, r8),
             Instruction::LDr16n16(r16, n16) => write!(f, "LD {},{}", r16, n16),
-
+            Instruction::Nop() => write!(f, "NOP"),
             Instruction::Unimplemented(op) => write!(f, "unimplemented {}", op),
         }
     }
@@ -240,6 +268,94 @@ impl Cpu {
         return match opcode {
             0x00 => Instruction::Nop(),
 
+            0x03 => Instruction::INC16(Reg16::BC),
+            0x13 => Instruction::INC16(Reg16::DE),
+            0x23 => Instruction::INC16(Reg16::HL),
+            0x33 => Instruction::INC16(Reg16::SP),
+            0x04 => Instruction::INC8(Reg8::B),
+            0x14 => Instruction::INC8(Reg8::D),
+            0x24 => Instruction::INC8(Reg8::H),
+            0x05 => Instruction::DEC8(Reg8::B),
+            0x15 => Instruction::DEC8(Reg8::D),
+            0x25 => Instruction::DEC8(Reg8::H),
+            0x0C => Instruction::INC8(Reg8::C),
+            0x1C => Instruction::INC8(Reg8::E),
+            0x2C => Instruction::INC8(Reg8::L),
+            0x3C => Instruction::INC8(Reg8::A),
+            0x0D => Instruction::DEC8(Reg8::C),
+            0x1D => Instruction::DEC8(Reg8::E),
+            0x2D => Instruction::DEC8(Reg8::L),
+            0x3D => Instruction::DEC8(Reg8::A),
+
+            0x0B => Instruction::DEC16(Reg16::BC),
+            0x1B => Instruction::DEC16(Reg16::DE),
+            0x2B => Instruction::DEC16(Reg16::HL),
+            0x3B => Instruction::DEC16(Reg16::SP),
+
+            0x80 => Instruction::ADDr8r8(Reg8::A, Reg8::B),
+            0x81 => Instruction::ADDr8r8(Reg8::A, Reg8::C),
+            0x82 => Instruction::ADDr8r8(Reg8::A, Reg8::D),
+            0x83 => Instruction::ADDr8r8(Reg8::A, Reg8::E),
+            0x84 => Instruction::ADDr8r8(Reg8::A, Reg8::H),
+            0x85 => Instruction::ADDr8r8(Reg8::A, Reg8::L),
+            0x87 => Instruction::ADDr8r8(Reg8::A, Reg8::A),
+            0x90 => Instruction::SUB(Reg8::B),
+            0x91 => Instruction::SUB(Reg8::C),
+            0x92 => Instruction::SUB(Reg8::D),
+            0x93 => Instruction::SUB(Reg8::E),
+            0x94 => Instruction::SUB(Reg8::H),
+            0x95 => Instruction::SUB(Reg8::L),
+            0x97 => Instruction::SUB(Reg8::A),
+            0xA0 => Instruction::AND(Reg8::B),
+            0xA1 => Instruction::AND(Reg8::C),
+            0xA2 => Instruction::AND(Reg8::D),
+            0xA3 => Instruction::AND(Reg8::E),
+            0xA4 => Instruction::AND(Reg8::H),
+            0xA5 => Instruction::AND(Reg8::L),
+            0xA7 => Instruction::AND(Reg8::A),
+            0xB0 => Instruction::OR(Reg8::B),
+            0xB1 => Instruction::OR(Reg8::C),
+            0xB2 => Instruction::OR(Reg8::D),
+            0xB3 => Instruction::OR(Reg8::E),
+            0xB4 => Instruction::OR(Reg8::H),
+            0xB5 => Instruction::OR(Reg8::L),
+            0xB7 => Instruction::OR(Reg8::A),
+            0x88 => Instruction::ADC(Reg8::B),
+            0x89 => Instruction::ADC(Reg8::C),
+            0x8A => Instruction::ADC(Reg8::D),
+            0x8B => Instruction::ADC(Reg8::E),
+            0x8C => Instruction::ADC(Reg8::H),
+            0x8D => Instruction::ADC(Reg8::L),
+            0x8F => Instruction::ADC(Reg8::A),
+            0x98 => Instruction::SBC(Reg8::B),
+            0x99 => Instruction::SBC(Reg8::C),
+            0x9A => Instruction::SBC(Reg8::D),
+            0x9B => Instruction::SBC(Reg8::E),
+            0x9C => Instruction::SBC(Reg8::H),
+            0x9D => Instruction::SBC(Reg8::L),
+            0x9F => Instruction::SBC(Reg8::A),
+            0xA8 => Instruction::XOR(Reg8::B),
+            0xA9 => Instruction::XOR(Reg8::C),
+            0xAA => Instruction::XOR(Reg8::D),
+            0xAB => Instruction::XOR(Reg8::E),
+            0xAC => Instruction::XOR(Reg8::H),
+            0xAD => Instruction::XOR(Reg8::L),
+            0xAF => Instruction::XOR(Reg8::A),
+            0xB8 => Instruction::CP(Reg8::B),
+            0xB9 => Instruction::CP(Reg8::C),
+            0xBA => Instruction::CP(Reg8::D),
+            0xBB => Instruction::CP(Reg8::E),
+            0xBC => Instruction::CP(Reg8::H),
+            0xBD => Instruction::CP(Reg8::L),
+            0xBF => Instruction::CP(Reg8::A),
+
+            0x09 => Instruction::ADDr16r16(Reg16::HL, Reg16::BC),
+            0x19 => Instruction::ADDr16r16(Reg16::HL, Reg16::DE),
+            0x29 => Instruction::ADDr16r16(Reg16::HL, Reg16::HL),
+            0x39 => Instruction::ADDr16r16(Reg16::HL, Reg16::SP),
+
+            0xE8 => Instruction::ADDr16n8(Reg16::SP, self.load_pc_inc()),
+
             0x36 => Instruction::LDHLn(self.load_pc_inc()),
 
             0x02 => Instruction::LDr16r8(Reg16::BC, Reg8::A),
@@ -335,7 +451,79 @@ impl Cpu {
     }
     fn execute(&mut self, inst: &Instruction) {
         match inst {
-            Instruction::Nop() => (),
+            Instruction::ADDr8r8(r1, r2) => {
+                self.set_reg8(*r1, self.reg8(*r1) + self.reg8(*r2))
+                // TODO: flags
+            }
+            Instruction::ADDr16r16(r1, r2) => {
+                self.set_reg16(*r1, self.reg16(*r1) + self.reg16(*r2))
+                // TODO: flags
+            }
+            Instruction::ADDr16n8(r16, n8) => {
+                self.set_reg16(*r16, self.reg16(*r16) + (*n8 as u16))
+                // TODO: flags
+            }
+            Instruction::SUB(r) => {
+                self.reg_a = self.reg_a - self.reg8(*r)
+                // TODO: flags
+            }
+            Instruction::ADC(r) => {
+                self.reg_a = self.reg_a + self.reg8(*r)
+                // TODO: carry flag?
+                // TODO: flags
+            }
+            Instruction::SBC(r) => {
+                self.reg_a = self.reg_a - self.reg8(*r)
+                // TODO: carry flag?
+                // TODO: flags
+            }
+            Instruction::CP(r) => {
+                if self.reg_a == self.reg8(*r) {
+                    self.reg_a = 0
+                } else {
+                    self.reg_a = 1
+                }
+                // TODO: flags
+            }
+
+            Instruction::AND(r) => {
+                if (self.reg_a != 0) && (self.reg8(*r) != 0) {
+                    self.reg_a = 1
+                } else {
+                    self.reg_a = 0
+                }
+            }
+            Instruction::OR(r) => {
+                if (self.reg_a != 0) || (self.reg8(*r) != 0) {
+                    self.reg_a = 1
+                } else {
+                    self.reg_a = 0
+                }
+            }
+            Instruction::XOR(r) => {
+                if (self.reg_a != 0) != (self.reg8(*r) != 0) {
+                    self.reg_a = 1
+                } else {
+                    self.reg_a = 0
+                }
+            }
+            Instruction::INC8(r) => {
+                self.set_reg8(*r, self.reg8(*r) + 1)
+                // TODO: flags
+            }
+            Instruction::DEC8(r) => {
+                self.set_reg8(*r, self.reg8(*r) - 1)
+                // TODO: flags
+            }
+            Instruction::INC16(r) => {
+                self.set_reg16(*r, self.reg16(*r) + 1)
+                // TODO: flags
+            }
+            Instruction::DEC16(r) => {
+                self.set_reg16(*r, self.reg16(*r) - 1)
+                // TODO: flags
+            }
+
             Instruction::LD(r1, r2) => {
                 self.set_reg8(*r1, self.reg8(*r2));
             }
@@ -367,6 +555,7 @@ impl Cpu {
             Instruction::LDr16n16(r16, n16) => {
                 self.set_reg16(*r16, *n16);
             }
+            Instruction::Nop() => (),
 
             // not implemented
             Instruction::Unimplemented(_op) => (),
